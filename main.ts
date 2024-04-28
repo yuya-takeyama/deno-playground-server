ab1f5a41f7ff4fe2f171b3f6142106363d6fd587
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-04-29 02:04:33 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,9 @@ const keys = ['access_counter'];
 serve(async function handleRequest(request: Request): Promise<Response> {
   const counter = await kv.get(keys);
   console.log(`counter: ${JSON.stringify(counter)}`);
+  console.log(`counter.value: ${JSON.stringify(counter.value)}`);
   const count = Number(counter.value) + 1;
-  console.log(`count: ${JSON.stringify(count)}`)
+  console.log(`count: ${JSON.stringify(count)}`);
   
   await kv.set(keys, count);
 
