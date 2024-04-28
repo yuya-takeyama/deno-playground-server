ba208c3ef33f81d1274ed5badd7cb9a9100fc8ff
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-04-29 01:58:08 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-Deno.serve((req: Request) => new Response("Hello World, This is Real. Don't you understand?"));
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const kv = await Deno.openKv();
+const keys = ['access_counter'];
+
+serve(async function handleRequest(request: Request): Promise<Response> {
+  const counter = await kv.get([keys]);
+  const count = Number(counter) + 1;
+  
+  await kv.set(keys, count);
+
+  return new Response(`This page has been visited ${count} times.`);
+});
