8255d457f8a6a19cef563c87e9ee36648ec3accd
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-04-29 02:10:11 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,16 @@ const kv = await Deno.openKv();
 const keys = ['access_counter'];
 
 serve(async function handleRequest(request: Request): Promise<Response> {
-  const counter = await kv.get(keys);
-  console.log(`counter: ${JSON.stringify(counter)}`);
-  console.log(`counter.value: ${JSON.stringify(counter.value)}`);
-  const count = Number(counter.value) || 0;
-  const newCount = count + 1;
-  console.log(`newCount: ${JSON.stringify(newCount)}`);
-  
-  await kv.set(keys, newCount);
+  const url = new URL(request.url);
 
-  return new Response(`This page has been visited ${count} times.`);
+  if (url.pathname === "/") {
+    const counter = await kv.get(keys);
+    const count = Number(counter.value) || 0;
+    const newCount = count + 1;
+    await kv.set(keys, newCount);
+
+    return new Response(`This page has been visited ${count} times.`);
+  } else {
+    return new Response("404 Not Found", { status: 404 });
+  }
 });
